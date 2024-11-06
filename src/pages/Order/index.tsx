5958c5559d738ee3bc2c50b2cb0eb9b88752530f
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2024-11-06 16:13:13 +0600

    - order page complete

--- src/pages/Order/index.tsx
+++ src/pages/Order/index.tsx
@@ -1,5 +1,29 @@
+import CartItem from '@/components/Cart/CartItem'
+
 const Order = () => {
-  return <div>Order</div>
+  return (
+    <section>
+      <header className='bg-[#9538E2] py-8'>
+        <div className='con flex flex-col items-center text-white'>
+          <h1 className='text-3xl font-bold'>Your Orders</h1>
+          <p className='mt-2 max-w-[65ch] text-center leading-7'>
+            Here is the list of items that you ordered since ever.
+          </p>
+        </div>
+      </header>
+
+      <div className='con mt-10'>
+        <ul className='flex flex-col items-center gap-4'>
+          <li className='w-full flex justify-center'>
+            <CartItem />
+          </li>
+          <li className='w-full flex justify-center'>
+            <CartItem />
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export default Order
